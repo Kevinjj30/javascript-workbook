8b8bf93fb85c68c82685b9b419862943a86fe39d
kevinjj30 <kevinjenkins56@yahoo.com>
2019-08-13 11:14:14 -0500

    dodgeball commit

--- 07week/api.js
+++ 07week/api.js
@@ -1,62 +1,26 @@
-// // document.getElementById("getAPI").addEventListener('click', getAPI)
-// const ul = document.getElementById("authors"); // Get the list where we will place our authors
-// const url = "https://randomuser.me/api/?results=10";
-// let data = {
-// 	name: "Sara"
-// };
-// // The parameters we are gonna pass to the fetch function
-// let fetchData = {
-// 	method: "POST",
-// 	body: data,
-// 	headers: new Headers()
-// };
-// fetch(url, fetchData);
-// function createNode(element) {
-// 	return document.createElement(element); // Create the type of element you pass in the parameters
-// }
+"use strict";
 
-// function append(parent, el) {
-// 	return parent.appendChild(el); // Append the second parameter(element) to the first one
-// }
-
-// // function getAPI(){
-// fetch("https://randomuser.me/api/")
-// 	.then(res => res.json())
-// 	.then(data => {
-// 		let authors = data.results; // Get the results
-// 		return authors.map(function(author) {
-// 			// Map through the results and for each run the code below
-// 			let li = createNode("li"), //  Create the elements we need
-// 				img = createNode("img"),
-// 				span = createNode("span");
-// 			img.src = author.picture.medium; // Add the source of the image to be the src of the img element
-// 			span.innerHTML = `${author.name.first} ${author.name.last}`; // Make the HTML of our span to be the first and last name of our author
-// 			append(li, img); // Append all our elements
-// 			append(li, span);
-// 			append(ul, li);
-// 		return authors
-// 		});
-// 	});
-
-// document.getElementById("getAPI").innerHTML = output;
-
-// document.getElementById("getAPI").addEventListener("click", getAPI);
-
+function catchError(result) {
+	if (!result.ok) {
+		throw Error(result.message);
+	}
+	return result;
+}
 function getAPI() {
-	fetch("https://randomuser.me/api/")
-		.then((res) => res.json())
-		.then((data) => {
-			let output = "<h2> Api Posts </h2>";
-			data.forEach(function(post) {
+	fetch("https://randomuser.me/api/?results=500")
+		.then(result => result.json())
+		.then(data => {
+			let output = `<h2>Friends</h2>`;
+			data.results.forEach(function(user) {
 				output += `
-
-			<ul>
-			<li> Title: ${post.title}</li>
-			<li> Body: ${post.title}</li>
-			</ul>
-		`;
+          <img src = "${user.picture.medium}" alt = "faces">
+              <ul>
+                  <li>${user.name.first} ${user.name.last}</li>
+                  <li>${user.location.city}, ${user.location.state}</li>
+                  <li>${user.email}</li>
+              </ul>
+          `;
 			});
-
-			document.getElementById("input").innerHTML = output;
+			document.getElementById("container").innerHTML = output;
 		});
 }
